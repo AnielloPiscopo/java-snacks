fb4532ff213b9d10bb84c0b05f669492aeb9c99a
AnielloPiscopo <aniello.piscopo0707@gmail.com>
2023-05-05 11:41:34 +0200

    Finished the third snack

--- src/org/snacks/java/Snack3.java
+++ src/org/snacks/java/Snack3.java
@@ -1,5 +1,15 @@
 package org.snacks.java;
 
 public class Snack3 {
-
+	public static void main(String[] args) {
+		int[] nums = {1,2,3,4,5,6};
+		int soms = 0;
+		
+		for(int i=0 ; i<nums.length ; i++) {
+			int num = nums[i];
+			soms = (i%2 == 0) ? soms : soms + num;
+		}
+		
+		System.out.println(soms);
+	}
 }
