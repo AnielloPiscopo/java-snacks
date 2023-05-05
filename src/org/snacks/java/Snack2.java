430e5943afd9bd9783547698d1081358780be691
AnielloPiscopo <aniello.piscopo0707@gmail.com>
2023-05-05 11:35:38 +0200

    Finished the second snack

--- src/org/snacks/java/Snack2.java
+++ src/org/snacks/java/Snack2.java
@@ -1,5 +1,24 @@
 package org.snacks.java;
 
-public class Snack2 {
+import java.util.Random;
 
+public class Snack2 {
+	public static void main(String[] args) {
+		String[] names = {"nome" , "nome" , "nome" , "nome" , "nome"};
+		String[] surnames = {"cognome" , "cognome" , "cognome"};
+		
+		Random rand = new Random();
+		
+		int namesListLength = names.length;
+		int surnamesListLength = surnames.length;
+		
+		int maxValue = (namesListLength > surnamesListLength) ? surnamesListLength : namesListLength;
+		
+		for(int i=0 ; i<maxValue; i++) {
+			int randomValue = rand.nextInt(maxValue);
+			String name = names[randomValue];
+			String surname = surnames[randomValue];
+			System.out.println(name +  " " + surname);
+		}
+	}
 }
