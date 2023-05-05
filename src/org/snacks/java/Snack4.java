d758cad5b0190194f5f491fcaaa3feb1773dc189
AnielloPiscopo <aniello.piscopo0707@gmail.com>
2023-05-05 11:54:41 +0200

    Finished the fourth snack

--- src/org/snacks/java/Snack4.java
+++ src/org/snacks/java/Snack4.java
@@ -1,5 +1,22 @@
 package org.snacks.java;
 
-public class Snack4 {
+import java.util.Scanner;
 
+public class Snack4 {
+	public static void main(String[] args) {
+		System.out.print("Inserisci la parola: ");
+		
+		Scanner in = new Scanner(System.in);
+		String word = in.nextLine().toLowerCase();
+		in.close();
+		
+		int wordLength = word.length();
+		String reversedWord = "";
+		
+		for(int i=wordLength-1 ; i>=0 ; i--) {
+			reversedWord += word.charAt(i);
+		}
+		
+		System.out.println((reversedWord.equals(word) ? "La parola è palindroma" : "La parola non è palindroma"));
+	}
 }
